b60bf8c753c5d36755e666cc1776e206befd0833
Michelle Lamelza <mishlamelza@gmail.com>
2023-08-05 04:56:35 -0400

    updated installed app

--- ReactDjango_JWT_StarterCode-main/backend/tvshows/admin.py
+++ ReactDjango_JWT_StarterCode-main/backend/tvshows/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Video
+from .models import Comment
+from .models import Reply
+
+# Register your models here.
+admin.site.register(Video)
+admin.site.register(Comment)
+admin.site.register(Reply)
 
 # Register your models here.
