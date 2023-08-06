f1f161953d59c1f001235474e32781a0c1470902
Michelle Lamelza <mishlamelza@gmail.com>
2023-08-06 15:57:06 -0400

    updated urls.py for comments app

--- ReactDjango_JWT_StarterCode-main/backend/comments/urls.py
+++ ReactDjango_JWT_StarterCode-main/backend/comments/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from comments import views
 
+# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
+
 urlpatterns = [
-    # Endpoint to create a new comment
-    path('create/', views.create_comment, name='create-comment'),
-    # Endpoint to retrieve all comments for a specific video
-    path('video/<str:video_id>/', views.get_comments_for_video, name='get-comments-for-video'),
-    # I may add more endpoints for other comment-related functionality
+    path('', views.user_comments),
+    path('all/', views.get_all_comments),
 ]
+
