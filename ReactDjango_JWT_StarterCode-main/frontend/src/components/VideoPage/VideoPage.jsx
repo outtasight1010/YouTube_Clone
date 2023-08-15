d3da9d0cb94edac118c3f70ae6d611f138618646
Michelle Lamelza <mishlamelza@gmail.com>
2023-08-15 03:16:14 -0400

    changed my "hard-coded videos" to YT API in VideoPage component

--- ReactDjango_JWT_StarterCode-main/frontend/src/components/VideoPage/VideoPage.jsx
+++ ReactDjango_JWT_StarterCode-main/frontend/src/components/VideoPage/VideoPage.jsx
@@ -1,29 +1,44 @@
-import react from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './VideoPage.css';
 
-const API = 'AIzaSyA48Og_bmU2yVzGDjrFc5pibu6EmwwJj0Y'
-var fetchurl =`https://www.googleapis.com/youtube/v3/search?key=${API}`
+const API_KEY = 'AIzaSyA48Og_bmU2yVzGDjrFc5pibu6EmwwJj0Y';
+const SEARCH_QUERY = 'rocky balboa';
 
+const VideoPage = () => {
+  const [videos, setVideos] = useState([]);
 
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${SEARCH_QUERY}`);
+        const videoData = response.data.items;
+        setVideos(videoData);
+      } catch (error) {
+        console.error('Error fetching videos:', error);
+      }
+    };
 
-export const YTvideos =() => {
-    return(
-        <div className = 'video-grid'>
-            <div className = "video-card">
-                <iframe width="200" height="150" src="https://www.youtube.com/embed/D_Vg4uyYwEk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>    
-            <div className = 'video-card'>
-                <iframe width="200" height="150" src="https://www.youtube.com/embed/RC6sMFbL288?start=95" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div> 
-            <div className = 'video-card'>
-                <iframe width="200" height="150" src="https://www.youtube.com/embed/TBDrI9h6Bqo?start=95" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div> 
-            <div className = 'video-card'>
-                <iframe width="200" height="150" src="https://www.youtube.com/embed/Fm5q-PAdBjg?start=95" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>  
-            <div className='video-card'>
-                <iframe width="200" height="150" src="https://www.youtube.com/embed/SDe3qE_aw8Q?start=95" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
+    fetchVideos();
+  }, []);
+
+  return (
+    <div className="video-grid">
+      {videos.map(video => (
+        <div key={video.id.videoId} className="video-card">
+          <iframe
+            width="200"
+            height="150"
+            src={`https://www.youtube.com/embed/${video.id.videoId}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
+
+export default VideoPage;
