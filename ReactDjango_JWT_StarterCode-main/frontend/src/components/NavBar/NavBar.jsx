b5632c41b498694b036dfb877bf2a380b0150ee9
Michelle Lamelza <mishlamelza@gmail.com>
2023-08-17 02:42:22 -0400

    Update NavBar.jsx

--- ReactDjango_JWT_StarterCode-main/frontend/src/components/NavBar/NavBar.jsx
+++ ReactDjango_JWT_StarterCode-main/frontend/src/components/NavBar/NavBar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       <ul>
         <li className="brand">
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>YouTube Clone</b>
+            <b>YouTube 4U</b>
           </Link>
         </li>
         <li>
