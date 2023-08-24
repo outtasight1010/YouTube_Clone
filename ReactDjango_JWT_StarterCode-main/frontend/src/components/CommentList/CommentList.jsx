bbbc619cd707de45d51e4e721c1412a1d8990ecf
Michelle Lamelza <mishlamelza@gmail.com>
2023-08-24 18:04:25 -0400

    added css to comments (from backend)

--- ReactDjango_JWT_StarterCode-main/frontend/src/components/CommentList/CommentList.jsx
+++ ReactDjango_JWT_StarterCode-main/frontend/src/components/CommentList/CommentList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './CommentList.css';
 
 const CommentList = ({ videoId }) => {
   const [comments, setComments] = useState([]);
